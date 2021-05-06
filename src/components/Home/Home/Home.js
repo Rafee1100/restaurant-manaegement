1dbd33559d97028dd9f6adee3fa9a7b2dee45b38
Genius15066 <amazinggenius72@gmail.com>
2021-05-06 15:31:23 +0600

    homepage design

---
+++ src/components/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tab from '../Tab/Tab';
+
+const Home = () => {
+    return (
+        <div>
+           <Tab/> 
+        </div>
+    );
+};
+
+export default Home;
